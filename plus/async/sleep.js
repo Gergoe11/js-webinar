74319194997c28e636208e65418c8d5b979ac859
Gergoe11 <99530535+Gergoe11@users.noreply.github.com>
2022-07-12 11:37:27 +0200

    task files update

--- plus/async/sleep.js
+++ plus/async/sleep.js
@@ -5,3 +5,33 @@
  * @returns {Promise} the promise which resolves when wait if over
  *                    or rejects if parameter is not correct
  */
+// should handle incorrect values
+// should wait at most 10 seconds"
+// should wait given seconds
+
+module.exports = async function sleep(sec) {
+    return new Promise((resolve, reject) => {
+        if(typeof sec === "number" ){
+    // let sec = 10;        
+            setTimeout (resolve, 1000*sec) 
+        } else {
+            reject(new Error("wrong input"))
+        }
+
+        
+    }
+)};
+
+
+
+/*
+ return new Promise((resolve, reject) => {
+        if(typeof sec !== "number" ){
+           reject throw new Error()
+        }
+        else if(sec !== 10){
+            throw new Error()
+        }
+
+        setTimeout(() => resolve(), 1000 *sec )
+        */

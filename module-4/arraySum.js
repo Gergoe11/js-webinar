fbc3b7f2485aa89909e0041c708abaf14c70c5c0
Gergoe11 <99530535+Gergoe11@users.noreply.github.com>
2022-07-14 09:15:55 +0200

    update

--- module-4/arraySum.js
+++ module-4/arraySum.js
@@ -1,9 +1,35 @@
 /**
- * It receives an array of strings, integers and
- * arrays, like itself.
+ * It receives an elementsay of strings, integers and
+ * elementsays, like itself.
  * Return the summary of all integers in it on
  * any level.
  *
- * @param {Array} elements
+ * @param {elementsay} elements
  * @returns {number} summary of all integers or 0 in other cases
  */
+//let elements= [1, 2, "3", 4, false, 6];
+
+
+    module.exports = function arraySum(elements){
+    
+    
+       
+    let sum = 0;
+    for(let i = 0; i < elements.length; i++){
+        if(typeof elements[i] === "number"){
+        sum = sum + elements[i];
+
+
+        } else if(Array.isArray(elements[i])){
+            sum = sum + arraySum(elements[i]);
+        }
+
+        
+    }
+    return sum;
+
+    }
+    
+
+    
+
